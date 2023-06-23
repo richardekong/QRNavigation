6da49d83444d9a66eee9834b9c6e91661bf19d8b
Faisal Alshathri <Faisal-sh@msn.com>
2023-06-23 11:21:28 +0100

    This commit made in order to implemnt the issue number 13 (#13) which is Create Spring Boot application which has two tasks whcih done also in this commit

---
+++ src/main/java/com/example/qrnavigation/QrNavigationApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrnavigation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrNavigationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrNavigationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/qrnavigation/QrNavigationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrnavigation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrNavigationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
