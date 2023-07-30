29b8e773e1bc625b36be6f707066c63ec77def76
Richard Willie Ekong <ekongr@cardiff.ac.uk>
2023-07-30 22:14:00 +0100

    Completion of QR Navigation System App Header (Tasks #140, #141 and #142 of Issue #139)
    - Write functional test case for the application's header fragment
    - Create the application header component as a reusable thymeleaf fragment
    - Create an external CSS file for this header fragment
    
    CHANGES WILL BE COMMITED TO:
    - src/main/java/com/example/qrnavigationproject/controller/DemoController.java
    - src/main/resources/static/css/qrNavigationHeader.css
    - src/main/resources/static/images/app_icon.png
    - src/main/resources/templates/qrNavHeaderTest.html
    - src/main/resources/templates/qrNavigationHeader.html
    - src/test/java/com/example/qrnavigationproject/pages/QRNavigationHeaderFunctionalTest.java
    
    Issue #139

---
+++ src/test/java/com/example/qrnavigationproject/pages/QRNavigationHeaderFunctionalTest.java
@@ -0,0 +1,106 @@
+package com.example.qrnavigationproject.pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static java.lang.String.format;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class QRNavigationHeaderFunctionalTest {
+
+    @Value("${local.server.port}")
+    private int port;
+    WebDriver driver;
+    private String pageURL;
+    private static final String APP_NAME = "QR NAVIGATION SYSTEM";
+
+    @BeforeAll
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setupTest() {
+        pageURL = format("http://localhost:%d/qrnavigation", port);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        //access the page
+        driver.get(pageURL);
+    }
+
+    @AfterEach
+    void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void verifyPresenceOfAppNameText() {
+        final WebElement spanElement = driver.findElement(By.id("app-name"));
+        assertNotNull(spanElement.getText());
+        assertEquals(spanElement.getText(), APP_NAME);
+        assertTrue(spanElement.isDisplayed());
+    }
+
+    @Test
+    public void verifyPresenceOfHeader() {
+        final WebElement headerElement = driver.findElement(By.tagName("header"));
+        assertNotNull(headerElement);
+        assertTrue(headerElement.isDisplayed());
+    }
+
+    @Test
+    public void verifyPresenceOfLogo() {
+        final WebElement imgElement = driver.findElement(By.id("app-logo"));
+        assertNotNull(imgElement);
+        assertFalse(imgElement.getAttribute("src").isEmpty());
+        assertTrue(imgElement.isDisplayed());
+    }
+
+    @Test
+    public void verifyPresenceOfCallToAction() {
+        final WebElement anchorElement = driver.findElements(By.className("auth-link")).get(0);
+        final WebElement childSpanElement = anchorElement.findElement(By.tagName("span"));
+        final WebElement childImgElement = anchorElement.findElement(By.tagName("img"));
+        final String loginURL = pageURL+"/login";
+        final String logoutURL = pageURL+"/logout";
+        final String link = anchorElement.getAttribute("href");
+        assertNotNull(anchorElement);
+        assertNotNull(link);
+        assertFalse(link.isEmpty());
+        assertTrue(link.equals(loginURL) || link.equals(logoutURL));
+
+        assertNotNull(childSpanElement);
+        assertFalse(childSpanElement.getText().isEmpty());
+        assertTrue(childSpanElement.getText().equals("Login") ||
+                childSpanElement.getText().equals("Logout"));
+        assertTrue(childSpanElement.isDisplayed());
+
+        assertNotNull(childImgElement);
+        assertNotNull(childImgElement.getAttribute("src"));
+        assertFalse(childImgElement.getAttribute("src").isEmpty());
+        assertTrue(childImgElement.isDisplayed());
+
+    }
+
+    @Test
+    public void verifyLoginSlashLoginTextToggle() {
+        //TODO latter
+        System.out.println("TODO latter");
+    }
+}
