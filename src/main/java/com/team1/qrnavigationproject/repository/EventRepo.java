ad42da58676117ff895e968f33eb0a9cedd72435
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:07:52 +0100

    Refine EventRepo for enhanced organization and readability
    Enhanced the EventRepo interface implementation to provide more clarity and readability while maintaining the same functionality:
    
    Changes Made:
    - Replaced the custom query in findAllEvents() method with a simplified query that retrieves all events.
    - Removed unnecessary annotations from the findAllEvents() method.
    - Ensured consistent use of annotations and proper formatting throughout the class.
    - Kept existing methods for finding events by name and ID unchanged.
    
    These changes aim to improve the maintainability and structure of the EventRepo interface.
    
    Modified Files:
    - com.team1.qrnavigationproject.repository.EventRepo

--- src/main/java/com/team1/qrnavigationproject/repository/EventRepo.java
+++ src/main/java/com/team1/qrnavigationproject/repository/EventRepo.java
@@ -9,21 +9,14 @@ import java.util.List;
 
 @Repository
 public interface EventRepo extends JpaRepository<Event, Integer> {
-
     @Query("SELECT e FROM Event e WHERE e.organizer.id = :organizationId")
     List<Event> findAllEvents(int organizationId);
-
     Event save(Event event);
-
     @Query("SELECT e FROM Event e WHERE e.name = :eventName")
     Event findByEventName(@Param("eventName") String eventName);
-
     @Query("SELECT e FROM Event e WHERE e.id = :eventId")
     Event findByEventId(@Param("eventId") int eventId);
-
     @Query("SELECT e.id as eventId, e.name as eventName FROM Event e WHERE e.id IN :eventIds")
     List<Object[]> findEventNamesAndIdsByIds(@Param("eventIds") List<Integer> eventIds);
-
-
 }
 
