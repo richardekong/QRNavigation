b3567b2d935f70515ad40b93d8ed4ebe7357ef54
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:10:14 +0100

    Refine SpaceRepo for better readability and consistency
    Optimized the SpaceRepo interface implementation to improve code readability and consistency:
    
    Changes Made:
    - Simplified the class structure by removing the unnecessary 'Space findById(int id)' method.
    - Maintained the 'findSpaceByName' method for finding spaces by name.
    - Ensured consistent use of annotations and formatting throughout the class.
    
    These changes aim to enhance the SpaceRepo interface while preserving its original functionality.
    
    Modified Files:
    - com.team1.qrnavigationproject.repository.SpaceRepo

--- src/main/java/com/team1/qrnavigationproject/repository/SpaceRepo.java
+++ src/main/java/com/team1/qrnavigationproject/repository/SpaceRepo.java
@@ -3,13 +3,10 @@ package com.team1.qrnavigationproject.repository;
 import com.team1.qrnavigationproject.model.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
 @Repository
 public interface SpaceRepo extends JpaRepository<Space, Integer> {
-
     Optional<Space> findSpaceByName(String spaceName);
-
     Space findById(int id);
 }
