9d2db114adedae5ff80f44006558d09713eb13d5
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:19:20 +0100

    Implement ContentService for content management operations
    Implemented the ContentService interface to provide a structured set of methods for managing content-related operations:
    - Added methods for retrieving distinct content IDs associated with an organization.
    - Included methods for retrieving content details along with event, subSpace, and space names.
    - Provided methods for retrieving content details based on event ID.
    - Added methods for saving, updating, and deleting content.
    - Maintained methods for finding content by name and ID.
    
    These changes introduce a well-defined interface to manage content-related actions.
    
    Modified Files:
    - com.team1.qrnavigationproject.service.ContentService

--- src/main/java/com/team1/qrnavigationproject/service/ContentService.java
+++ src/main/java/com/team1/qrnavigationproject/service/ContentService.java
@@ -1,12 +1,12 @@
 package com.team1.qrnavigationproject.service;
 
 import com.team1.qrnavigationproject.model.Content;
-
 import java.util.List;
 
 public interface ContentService {
     List<Integer> findDistinctContentIds(int organizationId);
     List<Object[]> findAll(int organizationId);
+    List<Object[]> getContentsByEventId(int eventId);
     Content saveContent(Content content);
     Content findContentByName(String contentName);
     Content findContentById(int contentId);
