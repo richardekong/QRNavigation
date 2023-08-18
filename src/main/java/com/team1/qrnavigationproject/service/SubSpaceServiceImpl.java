aed9ccecc69bf0a00c14b872eb3d8e3a2a98905f
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:40:56 +0100

    Implement SubSpaceService for managing subspaces
    Implemented the SubSpaceService interface to provide concrete implementations for managing subspaces in the application. The class utilizes the SubSpaceRepo repository for data access and manipulation. Methods to retrieve, save, update, and delete subspaces have been implemented. Additionally, methods for fetching subspaces by their names and IDs have been provided for enhanced querying capabilities.
    
    Modified Files:
    - com.team1.qrnavigationproject.service.SubSpaceServiceImpl

--- src/main/java/com/team1/qrnavigationproject/service/SubSpaceServiceImpl.java
+++ src/main/java/com/team1/qrnavigationproject/service/SubSpaceServiceImpl.java
@@ -4,50 +4,40 @@ import com.team1.qrnavigationproject.model.SubSpace;
 import com.team1.qrnavigationproject.repository.SubSpaceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class SubSpaceServiceImpl implements SubSpaceService {
-
     private final SubSpaceRepo subSpaceRepo;
-
     @Autowired
     public SubSpaceServiceImpl(SubSpaceRepo subSpaceRepo) {
         this.subSpaceRepo = subSpaceRepo;
     }
-
     @Override
     public List<SubSpace> getAllSubSpaces() {
         return subSpaceRepo.findAll();
     }
-
     @Override
     public Optional<SubSpace> getSubSpaceById(int id) {
         return Optional.ofNullable(subSpaceRepo.findById(id));
     }
-
     @Override
     public SubSpace saveSubSpace(SubSpace subSpace) {
         return subSpaceRepo.save(subSpace);
     }
-
     @Override
     public void deleteSubSpace(int id) {
         subSpaceRepo.deleteById(id);
     }
-
     @Override
     public Optional<SubSpace> getSubSpaceByName(String subSpaceName) {
         return subSpaceRepo.findSubSpaceByName(subSpaceName);
     }
-
     @Override
     public List<SubSpace> getSubspacesBySpaceId(Integer spaceId){
         return subSpaceRepo.getSubspacesBySpaceId(spaceId);
     }
-
     @Override
     public SubSpace findById(int id){ return subSpaceRepo.findById(id);}
 }
