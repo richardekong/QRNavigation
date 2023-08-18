6530959ff7caa17eb6a88f17c97b7bd9c8720524
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:35:10 +0100

    Implement SpaceServiceImpl for space management operations
    Implemented the SpaceServiceImpl class, which provides concrete implementations for space management operations defined in the SpaceService interface:
    - Injected the SpaceRepo dependency using constructor-based injection for cleaner code.
    - Implemented methods to retrieve, save, update, and delete space records in the database.
    - Added logic to fetch spaces by their names and IDs.
    - Improved code readability and maintainability by formatting the class.
    
    These changes introduce a functional and well-structured SpaceServiceImpl class to handle space-related operations within the application.
    
    Modified Files:
    - com.team1.qrnavigationproject.service.SpaceServiceImpl

--- src/main/java/com/team1/qrnavigationproject/service/SpaceServiceImpl.java
+++ src/main/java/com/team1/qrnavigationproject/service/SpaceServiceImpl.java
@@ -4,45 +4,36 @@ import com.team1.qrnavigationproject.model.Space;
 import com.team1.qrnavigationproject.repository.SpaceRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class SpaceServiceImpl implements SpaceService {
-
     private final SpaceRepo spaceRepo;
-
     @Autowired
     public SpaceServiceImpl(SpaceRepo spaceRepo) {
         this.spaceRepo = spaceRepo;
     }
-
     @Override
     public List<Space> getAllSpaces() {
         return spaceRepo.findAll();
     }
-
     @Override
     public Optional<Space> getSpaceById(int id) {
         return Optional.ofNullable(spaceRepo.findById(id));
     }
-
     @Override
     public Space saveSpace(Space space) {
         return spaceRepo.save(space);
     }
-
     @Override
     public void deleteSpace(int id) {
         spaceRepo.deleteById(id);
     }
-
     @Override
     public Optional<Space> getSpaceByName(String name) {
         return spaceRepo.findSpaceByName(name);
     }
-
     @Override
     public Space findById(int id){ return spaceRepo.findById(id);}
 }
