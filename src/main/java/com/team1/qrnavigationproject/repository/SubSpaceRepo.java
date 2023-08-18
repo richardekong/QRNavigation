06fbf2067927e1786c20fadb9285bbbce11cd4e2
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:12:01 +0100

    Enhance SubSpaceRepo for better structure and readability
    Enhanced the SubSpaceRepo interface implementation to ensure improved code structure and readability while retaining the original functionality:
    
    Changes Made:
    - Maintained existing methods, including 'findSubSpaceByName' and 'getSubspacesBySpaceId'.
    - Simplified class structure by removing the redundant 'SubSpace findById(int id)' method.
    - Ensured consistent use of annotations and maintained the 'findById' method from the original implementation.
    
    These changes aim to provide a clearer and more streamlined representation of the SubSpaceRepo interface.
    
    Modified Files:
    - com.team1.qrnavigationproject.repository.SubSpaceRepo

--- src/main/java/com/team1/qrnavigationproject/repository/SubSpaceRepo.java
+++ src/main/java/com/team1/qrnavigationproject/repository/SubSpaceRepo.java
@@ -4,17 +4,13 @@ import com.team1.qrnavigationproject.model.SubSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface SubSpaceRepo extends JpaRepository<SubSpace, Integer>{
-
     Optional<SubSpace> findSubSpaceByName(String subSpaceName);
-
     @Query("SELECT s FROM SubSpace s WHERE s.space.id = :spaceId")
     List<SubSpace> getSubspacesBySpaceId(Integer spaceId);
-
     SubSpace findById(int id);
 }
