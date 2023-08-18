9291f669826242d59000033f74aa80d7a7939078
Faisal Alshathri <Faisal-sh@msn.com>
2023-08-18 19:37:47 +0100

    Define SubSpaceService interface for managing subspaces
    Defined the SubSpaceService interface to outline the contract for managing subspaces within the application. The interface specifies methods to retrieve, save, update, and delete subspaces. Additionally, methods for fetching subspaces by their names and IDs have been defined for more flexible querying.
    
    Modified Files:
    - com.team1.qrnavigationproject.service.SubSpaceService

--- src/main/java/com/team1/qrnavigationproject/service/SubSpaceService.java
+++ src/main/java/com/team1/qrnavigationproject/service/SubSpaceService.java
@@ -1,18 +1,14 @@
 package com.team1.qrnavigationproject.service;
-import com.team1.qrnavigationproject.model.SubSpace;
 
+import com.team1.qrnavigationproject.model.SubSpace;
 import java.util.List;
 import java.util.Optional;
-
 public interface SubSpaceService {
-
     List<SubSpace> getAllSubSpaces();
     Optional<SubSpace> getSubSpaceById(int id);
     SubSpace saveSubSpace(SubSpace subSpace);
     void deleteSubSpace(int id);
     Optional<SubSpace> getSubSpaceByName(String subSpaceName);
-
     List<SubSpace> getSubspacesBySpaceId(Integer spaceId);
-
     SubSpace findById(int id);
 }
